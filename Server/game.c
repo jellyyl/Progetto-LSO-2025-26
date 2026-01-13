f6c4cedac9a406617aad88a13c5dbdcb10a77660
Darknine2 <152090593+Darknine2@users.noreply.github.com>
2026-01-13 18:26:16 +0100

    update game.c aggiunto check_winner

--- Server/game.c
+++ Server/game.c
@@ -91,12 +91,12 @@ void create_game(int client_id)
     insert_game_into_vector(&game_vector, &new_game);
 }
 
-/*
+
 
 void join_game(int client_id, int game_id, int sd)
 {
 
-    Game *selected_game = find_game_by_id(game_id);
+    Game *selected_game = get_game_by_id(game_id);
 
     if(selected_game == NULL)
     {
@@ -143,7 +143,7 @@ void join_game(int client_id, int game_id, int sd)
 void approve_join_request(int game_id, int sd, int response)
 {
 
-    Game *selected_game = find_game_by_id(game_id);
+    Game *selected_game = get_game_by_id(game_id);
 
     if(selected_game == NULL)
     {
@@ -164,6 +164,7 @@ void approve_join_request(int game_id, int sd, int response)
             selected_game->state = ST_WAITING;
         }
 
+        //libera il thread del giocatore in attesa della risposta
         pthread_cond_signal(&selected_game->cond_approve);
     }
 
@@ -208,8 +209,45 @@ void move(int client_id, int game_id, int sd)
     pthread_mutex_unlock(selected_game->game_mutex);
     
 }
-*/
 
+int check_winner(Game* game)
+{
+    // Controlla righe e colonne
+    for (int i = 0; i < 3; i++)
+    {
+        if (game->table[i][0] == game->table[i][1] && game->table[i][1] == game->table[i][2] && game->table[i][0] != ' ')
+            return (game->table[i][0] == 'X') ? game->id_player1 : game->id_player2;
+        if (game->table[0][i] == game->table[1][i] && game->table[1][i] == game->table[2][i] && game->table[0][i] != ' ')
+            return (game->table[0][i] == 'X') ? game->id_player1 : game->id_player2;
+    }
+
+    // Controlla diagonali
+    if (game->table[0][0] == game->table[1][1] && game->table[1][1] == game->table[2][2] && game->table[0][0] != ' ')
+        return (game->table[0][0] == 'X') ? game->id_player1 : game->id_player2;
+    if (game->table[0][2] == game->table[1][1] && game->table[1][1] == game->table[2][0] && game->table[0][2] != ' ')
+        return (game->table[0][2] == 'X') ? game->id_player1 : game->id_player2;
+
+    // Controlla pareggio
+    int empty_found = 0;
+    for (int r = 0; r < 3; r++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            if (game->table[r][c] == ' ')
+            {
+                empty_found = 1;
+                break;
+            }
+        }
+        if (empty_found)
+            break;
+    }
+
+    if (!empty_found)
+        return 0; // Pareggio
+
+    return -1; // Nessun vincitore ancora
+}
 
 Game generate_game(int client_id)
 {
